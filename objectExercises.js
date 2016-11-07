4f1938765dbf41fda1a881f103fcc97e92acd2fe
Kirk Abbott <kirkabbott1@gmail.com>
2016-11-07 12:58:50 -0500

    updates exercises

--- objectExercises.js
+++ objectExercises.js
@@ -22,17 +22,50 @@
 // }
 // hate.apply(pic1);
 // hate.apply(pic2);
+// function showInfo() {
+//   console.log()
+// }
+// var mom = {
+//   firstName: 'Alice',
+//   lastName: 'Wong',
+//   eyeColor: 'brown',
+//   hairColor: 'black'
+//
+// };
+// var daughter = {
+//   firstName: 'Ilene',
+//   hairColor: 'brown'
+// };
+// daughter.__proto__ = mom;
+// console.log(daughter.firstName, daughter.lastName, daughter.eyeColor, daughter.hairColor);
 
-var mom = {
-  firstName: 'Alice',
-  lastName: 'Wong',
-  eyeColor: 'brown',
-  hairColor: 'black'
+// function Character () {
+// }
+//
+// Character.prototype.attack = function(enemy) {
+//   enemy.health -= this.power;
+// };
+// function Hero() {
+//   this.name = 'Hero';
+//   this.health = 10;
+//   this.power = 5;
+// }
+// Hero.prototype = Object.create(Character.prototype);
+//
+// Hero.prototype.attack = function(enemy) {
+//   if (Math.random < .2) {
+//     enemy.health -= this.power * 2;
+// } else {
+//   enemy.health -= this.power;
+//  };
+  // };
+// var Json = new Hero();
+// Jason.attack();
 
+function Person(name) {
+  this.name = name;
+}
+
+Person.prototype.greet = function(other) {
+  console.log('Hello ' + other.name + '! My name is ' + this.name + '.');
 };
-var daughter = {
-  firstName: 'Ilene',
-  hairColor: 'brown'
-};
-daughter.__proto__ = mom;
-console.log(daughter.firstName, daughter.lastName, daughter.eyeColor, daughter.hairColor);
